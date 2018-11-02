25edb936f1bc7ff0a12eea2e6e400ef75f32a83c
Maximilian Held <info@maxheld.de>
2018-11-02 18:59:51 +0100

    write to tmp

--- encrypt.sh
+++ encrypt.sh
@@ -1,4 +1,4 @@
 # this is to be run only locally on maxs computer, where the respective keys can be found
 # as per https://oncletom.io/2016/travis-ssh-deploy/
 travis login --com --auto
-travis encrypt-file ~/.ssh/karli_rsa --add before_deploy --com
+travis encrypt-file ~/.ssh/karli_rsa --add before_deploy --com --decrypt-to /tmp/karli_rsa
